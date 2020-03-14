70090dc88b83973d2d8d8e5883a2e0a10471b658
Alex Bryant <alexbryant710@gmail.com>
2020-03-14 23:16:16 +0000

    Linted py files

--- face_matcher.py
+++ face_matcher.py
@@ -1,5 +1,5 @@
 import face_recognition as fr
-import time # to measure operation time
+import time  # to measure operation time
 
 """
     FaceMatcher can 
@@ -10,13 +10,15 @@ import time # to measure operation time
         - cross match a list of images with another list of images (N:N)
         - write a csv file of results to a text file
 """
+
+
 class FaceMatcher:
-    
-    #  Method to initialise the class   
+
+    #  Method to initialise the class
     def __init__(self, tolerance=0.7):
-        self.tolerance = tolerance # This is acctually unused
-    
-    # Find faces in a single image   
+        self.tolerance = tolerance  # This is acctually unused
+
+    # Find faces in a single image
     #     image, Image file path
     #     match_model="cnn", mode of matching as per face_recogntion module spec
     def find_faces(self, image, match_model="cnn"):
@@ -25,8 +27,8 @@ class FaceMatcher:
         if face_locations:
             return face_locations
         return None
-    
-    # Find faces in a batch of images 
+
+    # Find faces in a batch of images
     #     input_list, list of image file paths
     #     do_log=False, log each on processed
     #     track_progress=200, log message after every batch of this size completed
@@ -44,43 +46,47 @@ class FaceMatcher:
                 print(num, "processed in", int(time.time() - start), "seconds")
             faces_found_col.append(face_found)
         finish_time = int(time.time() - start)
-        print("All completed. Average processing time of", (finish_time / len(input_list)), "seconds per image")
+        print("All completed. Average processing time of",
+              (finish_time / len(input_list)), "seconds per image")
         return faces_found_col
-    
+
     # Create a biometric template of the face and return it
     #     image_path, path to image to load
     #     location, precalulated face location
-    def create_template(self, image_path, location): # This is like a single enrollment
+    def create_template(self, image_path, location):  # This is like a single enrollment
         face_image = fr.load_image_file(image_path)
         location = tuple(map(lambda x: int(x), location))
-        template = fr.face_encodings(face_image, known_face_locations=[location])
+        template = fr.face_encodings(
+            face_image, known_face_locations=[location])
         return template
-    
+
     # Create a dictionary of templates, acting as bulk enrollment
     #     enrollment_list, a list of tuples, (index, [image_path, location])
     def enroll_batch(self, enr_list):
         start = time.time()
         enrollment_dict = {}
         for item in enr_list:
-            enrollment_dict[item[0]] = self.create_template(item[1][0], item[1][1])
+            enrollment_dict[item[0]] = self.create_template(
+                item[1][0], item[1][1])
             if(item[0] % 100 == 0):
                 print("Enrolled...", item[0])
-        print("Finished enrolling", len(enr_list), "in", int(time.time() - start), "seconds")
+        print("Finished enrolling", len(enr_list),
+              "in", int(time.time() - start), "seconds")
         return enrollment_dict
-        
+
     # Single match on two images
     #     encoding_1
     #     encoding_2
-    def single_match(self, encoding_1, encoding_2, threshold=0.6): 
-        # With the threshold we are inverting the score so higher thresholds mean harder to match 
-        # dissimilar pairs.        
+    def single_match(self, encoding_1, encoding_2, threshold=0.6):
+        # With the threshold we are inverting the score so higher thresholds mean harder to match
+        # dissimilar pairs.
         return fr.compare_faces(encoding_1, encoding_2[0], tolerance=(1 - threshold))
-    
+
     # Generate a score
     #     encoding_1
     #     encoding_2
     #     mate, tell the function that the ground truth says the two images are the same person
-    #     This is a recursive function that calls itself until a match is made. 
+    #     This is a recursive function that calls itself until a match is made.
     #     Do not give kwarg new_threshold on first iteration, but do say whether mate=True/False initially
     def generate_score(self, encoding_1, encoding_2, mate=True, start=True, new_threshold=1.0, logging=False):
         # When the threshold returns it represents the score where the images match
@@ -102,13 +108,13 @@ class FaceMatcher:
                 return "%.2f" % new_threshold
             threshold += 0.01
             return self.generate_score(encoding_1, encoding_2, new_threshold=threshold, mate=mate, start=False, logging=logging)
-        
+
     # Mini function to generate a CSV file of results
     def create_csv_file(self, file_name, arr):
         with open(file_name, 'w') as f:
             for item in arr:
-                f.write("%s\n" % item)  
-     
+                f.write("%s\n" % item)
+
     # Cross match a batch of images by id
     #     enrollments, dictionary of all enrollment with id as key and value as biometric template
     #     pairs, list of lists of pairs of ids
@@ -118,12 +124,11 @@ class FaceMatcher:
         scores = []
         scores.append("subject_id_1,subject_id_2,score")
         for count, pair in enumerate(pairs):
-            score = self.generate_score(enrollments[pair[0]], enrollments[pair[1]], mate=True, logging=False, new_threshold=False)
+            score = self.generate_score(
+                enrollments[pair[0]], enrollments[pair[1]], mate=True, logging=False, new_threshold=False)
             scores.append(str(pair[0]) + "," + str(pair[1]) + "," + str(score))
             if(count % 5000 == 0):
                 print(count, "matches completed")
-        print("Finished matching pairs in", int(time.time() - start), "seconds")
+        print("Finished matching pairs in", int(
+            time.time() - start), "seconds")
         self.create_csv_file(csv_output, scores)
-        
-        
-        
--- fr_data_extractor.py
+++ fr_data_extractor.py
@@ -24,27 +24,31 @@ import sys
     Additional column added: 
     N= relative file path of the image file from root dir of project
 """
+
+
 class FrDataExtractor:
-    
-    #  Method to initialise the class   
+
+    #  Method to initialise the class
     def __init__(self, columns, image_path='.', image_type='jpg'):
-        self.image_path = image_path      
-        self.image_type = image_type  
+        self.image_path = image_path
+        self.image_type = image_type
         self.columns = columns
 
     # Line formatting specific to format of data accumulated
     def __set_column_names(self, delimiter):
         # Return each lines with specified column and delimit with inputted delimiting char
-        return delimiter.join(self.columns) + ",image_path" # Also adding file_path as a column
-    
+        # Also adding file_path as a column
+        return delimiter.join(self.columns) + ",image_path"
+
     # Line formatting specific to spec
     def __format_line(self, line, delimiter):
         # First remove file path then replace all underscores with delimiting char
-        if(len(line.split("_")) == len(self.columns)): # Checking that the length matches input of columns (or ignore line)
+        # Checking that the length matches input of columns (or ignore line)
+        if(len(line.split("_")) == len(self.columns)):
             return line.replace('.' + self.image_type, "").replace("_", delimiter)
-        return None # Ignore rows that don't conform to the defined standard
-       
-    #  Read in data from the images directory specified in class instantiation   
+        return None  # Ignore rows that don't conform to the defined standard
+
+    #  Read in data from the images directory specified in class instantiation
     def __read_dir(self):
         try:
             path = self.image_path
@@ -54,29 +58,29 @@ class FrDataExtractor:
                 for file in f:
                     file_str = '.' + self.image_type
                     if file_str in file:
-                        files.append(os.path.join(r, file).replace(self.image_path + "/", ""))
-            return files # returns a list of file names in a dir of a certain file type
+                        files.append(os.path.join(r, file).replace(
+                            self.image_path + "/", ""))
+            return files  # returns a list of file names in a dir of a certain file type
         except IOError as e:
             print("I/O error", e)
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
-            
+
     # Generate CSV from list of file_names
-    # Only method available to call externally. This class should be adapted for different formats of data    
+    # Only method available to call externally. This class should be adapted for different formats of data
     def generate_csv(self, file_name, delimiter):
         try:
-            f = open(file_name, "w") # Open the file for writing
+            f = open(file_name, "w")  # Open the file for writing
             try:
                 f.write(self.__set_column_names(delimiter) + '\n')
                 for line in self.__read_dir():
                     formatted_line = self.__format_line(line, delimiter)
-                    #  line is the relative filepath of the file                            
+                    #  line is the relative filepath of the file
                     if formatted_line:
-                        f.write(formatted_line + delimiter + self.image_path + "/" + line + '\n')
+                        f.write(formatted_line + delimiter +
+                                self.image_path + "/" + line + '\n')
             finally:
-                f.close() # Close the file
+                f.close()  # Close the file
         except IOError as e:
             print('Could not generate CSV file')
-        
-        
